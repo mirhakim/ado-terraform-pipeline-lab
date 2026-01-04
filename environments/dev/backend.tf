8f79c4178263b29d9167eca3b37ea3709d2d8aa1
Your Name <you@example.com>
2026-01-04 15:55:27 +0530

    Initial commit - update8

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tf-backend"
+    resource_group_name  = "tf-backend-rg"
     storage_account_name = "tfbackend1767090006"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
